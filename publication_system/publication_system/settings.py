4c1a6b94ffd2608f7f90bf2a1b529c014af90ac8
Alexandr Pogrebnyuk <nani.lennauchpage@gmail.com>
2026-09-13 13:47:10 +0300

    Added database to container

--- publication_system/publication_system/settings.py
+++ publication_system/publication_system/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'publication_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='postgres'),
+        'USER': config('POSTGRES_USER', default='postgres'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': config('POSTGRES_HOST', default='postgres_db'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 
